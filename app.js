620b305f26559c00c6a97ace2ee3bd8a46e6dfe8
Kevenaxel <hectorsastre476@gmail.com>
2025-01-26 10:01:28 -0600

    new

---
+++ app.js
@@ -0,0 +1,14 @@
+//Creacion de Objetos. Hay muchas Formas
+let objeto1 = {
+    nombre:"Kevin",
+    apeliido:"Flores",
+    edad:"20"
+}
+let objeto2 = {
+    nombre:"Mauricio",
+    apeliido:"Flores",
+    edad:"20"
+}
+
+console.log(objeto1)
+console.log(objeto2)
